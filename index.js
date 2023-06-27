86323411fc2bdd1fe803e0e2b7d946f7caef676f
Anthony Huarneck <anthonyhuarneck@anthonys-air.myfiosgateway.com>
2023-06-26 20:12:43 -0400

    Creates container div.

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
+const header = document.createElement("header");
 const h1 = document.createElement("h1");
 const body = document.querySelector("body");
+const div = document.createElement("div");
+const form = document.createElement("form");
+const ul = document.createElement("ul");
+const li = document.createElement("li");
+const section = document.createElement("section");
+const main = document.createElement("main");
 
 h1.textContent = "Retro Game Inventory";
 
-body.append(h1)
+body.appendChild(header)
+header.appendChild(h1)
+body.appendChild(main)
+div.classList.add("container")
+main.appendChild(div)
+
