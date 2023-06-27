d57ea496801bea51d4e14fbfbe08ac6c66b1a729
Anthony Huarneck <anthonyhuarneck@anthonys-air.myfiosgateway.com>
2023-06-27 19:15:52 -0400

    Edits HTML file.

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-const header = document.createElement("header");
-const h1 = document.createElement("h1");
 const body = document.querySelector("body");
-const div = document.createElement("div");
-const form = document.createElement("form");
+const div = document.querySelector("div");
+const form = document.querySelector("form");
 const ul = document.createElement("ul");
 const li = document.createElement("li");
 const main = document.createElement("main");
@@ -10,12 +8,8 @@ const sideDiv = document.createElement("div");
 const mainDiv = document.createElement("div");
 const input = document.createElement("input");
 
-h1.textContent = "Retro Game Inventory";
 
-body.appendChild(header);
-header.appendChild(h1);
 body.appendChild(main);
-div.classList.add("container");
 main.appendChild(div);
 sideDiv.classList.add("sidebar");
 mainDiv.classList.add("main");
@@ -54,4 +48,35 @@ function gameTemplate(name, inStock, price, used, rarity) {
     strong.textContent = "Rarity: ";
     li.append(document.createElement("br"), strong, rarity);
   }
+  const remove = document.createElement("button");
+  remove.innerText = "Remove Contact";
+  console.log(remove);
+  li.append(remove);
+
+  remove.addEventListener("click", (event) => {
+    li.remove();
+  });
+
+  return li;
 }
+
+function generateGame(name, inStock, price, used, rarity) {
+  const li = gameTemplate(name, inStock, price, used, rarity);
+
+  const ul = document.querySelector("ul");
+  ul.append(li);
+}
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  let name = event.target.name.value;
+  let inStock = event.target.inStock.value;
+  let price = event.target.price.value;
+  let used = event.target.used.value;
+  let rarity = event.target.rarity.value;
+
+  generateGame(name, inStock, price, used, rarity);
+
+  form.reset();
+});
