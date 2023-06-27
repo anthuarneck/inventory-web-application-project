20f6b488739a012c77f267b442368553bfc13902
Anthony Huarneck <anthonyhuarneck@anthonys-air.myfiosgateway.com>
2023-06-26 21:46:46 -0400

    Sets up template function for games.

--- index.js
+++ index.js
@@ -5,14 +5,53 @@ const div = document.createElement("div");
 const form = document.createElement("form");
 const ul = document.createElement("ul");
 const li = document.createElement("li");
-const section = document.createElement("section");
 const main = document.createElement("main");
+const sideDiv = document.createElement("div");
+const mainDiv = document.createElement("div");
+const input = document.createElement("input");
 
 h1.textContent = "Retro Game Inventory";
 
-body.appendChild(header)
-header.appendChild(h1)
-body.appendChild(main)
-div.classList.add("container")
-main.appendChild(div)
+body.appendChild(header);
+header.appendChild(h1);
+body.appendChild(main);
+div.classList.add("container");
+main.appendChild(div);
+sideDiv.classList.add("sidebar");
+mainDiv.classList.add("main");
+div.appendChild(mainDiv);
+div.appendChild(sideDiv);
 
+mainDiv.append(form);
+form.append(input);
+input.type = "text";
+input.placeholder = "x";
+
+function gameTemplate(name, inStock, price, used, rarity) {
+  const li = document.createElement("li");
+  li.textContent += name;
+
+  if (inStock) {
+    const strong = document.createElement("strong");
+    strong.textContent = "In Stock: ";
+    li.append(document.createElement("br"), strong, inStock);
+  }
+
+  if (price) {
+    const strong = document.createElement("strong");
+    strong.textContent = "Price: ";
+    li.append(document.createElement("br"), strong, price);
+  }
+
+  if (used) {
+    const strong = document.createElement("strong");
+    strong.textContent = "New/Used: ";
+    li.append(document.createElement("br"), strong, used);
+  }
+
+  if (rarity) {
+    const strong = document.createElement("strong");
+    strong.textContent = "Rarity: ";
+    li.append(document.createElement("br"), strong, rarity);
+  }
+}
