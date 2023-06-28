f58d4ee392307d0c9457d26200f8dcd63b519df0
Anthony Huarneck <anthonyhuarneck@anthonys-air.myfiosgateway.com>
2023-06-27 21:46:10 -0400

    Attempts adding image url feature to form.

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const body = document.querySelector("body");
 const div = document.querySelector("div");
 const form = document.querySelector("form");
-const ul = document.createElement("ul");
-const li = document.createElement("li");
+// const ul = document.querySelector("ul");
+// const li = document.querySelector("li");
 const main = document.createElement("main");
 const sideDiv = document.createElement("div");
 const mainDiv = document.createElement("div");
@@ -21,21 +21,27 @@ div.appendChild(sideDiv);
 // input.placeholder = "x";
 
 form.addEventListener("submit", (event) => {
-    event.preventDefault();
+  event.preventDefault();
 
-    let name = event.target.name.value
-    let price = event.target.price.value
-    let inStock = event.target.inStock.value
-    let used = event.target.price.value
-    let rarity = event.target.rarity.value
+  let image = event.target.image.value;
+  let name = event.target.name.value;
+  let price = `$${event.target.price.value}`;
+  let inStock = event.target.inStock;
+  let used = event.target.used.value;
+  let rarity = event.target.rarity.value;
 
-    generateGame(name, price, inStock, used, rarity)
+  generateGame(image, name, price, inStock, used, rarity);
 
-    form.reset();
-})
+  form.reset();
+});
 
-function gameTemplate(name, price, inStock, used, rarity) {
+function gameTemplate(image, name, price, inStock, used, rarity) {
   const li = document.createElement("li");
+
+  if (image) {
+    li.append(document.createElement("br"), image);
+  }
+
   li.textContent += name;
 
   if (price) {
@@ -59,8 +65,7 @@ function gameTemplate(name, price, inStock, used, rarity) {
     li.append(document.createElement("br"), strong, rarity);
   }
   const remove = document.createElement("button");
-  remove.innerText = "Remove Contact";
-  console.log(remove);
+  remove.innerText = "Remove Game";
   li.append(remove);
 
   remove.addEventListener("click", (event) => {
@@ -70,10 +75,9 @@ function gameTemplate(name, price, inStock, used, rarity) {
   return li;
 }
 
-function generateGame(name, price, inStock, used, rarity) {
-  const li = gameTemplate(name, price, inStock, used, rarity);
+function generateGame(image, name, price, inStock, used, rarity) {
+  const li = gameTemplate(image, name, price, inStock, used, rarity);
 
   const ul = document.querySelector("ul");
-  ul.append(li);
+  ul.append(document.createElement("br"), li);
 }
-
