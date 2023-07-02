0f622e062c544a871ded51dffa64f2f07008bec7
Anthony Huarneck <anthonyhuarneck@anthonys-air.myfiosgateway.com>
2023-07-01 20:46:06 -0400

    Adds various changes. Adds error messages when requirements for form are not met.

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const body = document.querySelector("body");
 const div = document.querySelector("div");
 const form = document.querySelector("form");
-// const ul = document.querySelector("ul");
-// const li = document.querySelector("li");
+const ul = document.querySelector("ul");
+const li = document.querySelector("li");
 const main = document.createElement("main");
 const sideDiv = document.createElement("div");
 const mainDiv = document.createElement("div");
 const input = document.createElement("input");
+const strong = document.querySelector("strong")
+const label = document.querySelector("label")
 
 body.appendChild(main);
 main.appendChild(div);
@@ -15,39 +17,41 @@ mainDiv.classList.add("main");
 div.appendChild(mainDiv);
 div.appendChild(sideDiv);
 
-// mainDiv.append(form);
-// form.append(input);
-// input.type = "text";
-// input.placeholder = "x";
+let errorMessage = document.createElement("p");
+errorMessage.style.color = "red";
+form.append(errorMessage)
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  let image = event.target.image.value;
   let name = event.target.name.value;
-  let price = `$${event.target.price.value}`;
+  let price = event.target.price.value;
   let stock = event.target.stock.value;
   let used = event.target.used.value;
   let rarity = event.target.rarity.value;
-
-  generateGame(image, name, price, stock, used, rarity);
-
-  form.reset();
-});
-
-function gameTemplate(image, name, price, stock, used, rarity) {
-  const li = document.createElement("li");
-
-  if (image) {
-    li.append(document.createElement("br"), image);
+  
+  if (!name) {
+    errorMessage.textContent = "Error: Game must contain a title";
+  } else if (!price) {
+    errorMessage.textContent = "Error: Game must contain a price";
+  } else if (!stock) {
+    errorMessage.textContent = "Error: Game must contain stock data";
+  } else {
+    errorMessage.textContent = "";
+    generateGame(name, price, stock, used, rarity);
   }
 
+form.reset();
+});
+
+function gameTemplate(name, price, stock, used, rarity) {
+  const li = document.createElement("li");
   li.textContent += name;
 
   if (price) {
     const strong = document.createElement("strong");
     strong.textContent = "Price: ";
-    li.append(document.createElement("br"), strong, price);
+    li.append(document.createElement("br"), strong, "$", price);
   }
   if (stock) {
     const strong = document.createElement("strong");
@@ -70,14 +74,27 @@ function gameTemplate(image, name, price, stock, used, rarity) {
 
   remove.addEventListener("click", (event) => {
     li.remove();
+
+  const update = document.createElement("button");
+  update.innerText = "Update Stock";
+
+
   });
 
   return li;
 }
 
-function generateGame(image, name, price, stock, used, rarity) {
-  const li = gameTemplate(image, name, price, stock, used, rarity);
+function generateGame(name, price, stock, used, rarity) {
+  const li = gameTemplate(name, price, stock, used, rarity);
 
   const ul = document.querySelector("ul");
   ul.append(document.createElement("br"), li);
 }
+
+let currentStock = "Yes"
+
+    li.addEventListener("click", (event) => {
+          console.log(li.innerText)
+        
+    })
+
